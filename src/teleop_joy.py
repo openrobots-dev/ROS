470c6d1d223ff4e545e753269638f5b3d9e0f41b
tinito <martino.migliavacca@gmail.com>
2013-10-11 19:16:31 +0200

    unified teleop node

--- src/teleop_joy.py
+++ src/teleop_joy.py
@@ -10,56 +10,63 @@ import rospy
 from sensor_msgs.msg import Joy
 from r2p.msg import Velocity
 
-robot = rospy.get_param('/robot')
+topic = rospy.get_param('topic', 'robot')
+setpoint_scale = rospy.get_param('setpoint_scale', {'x': 1, 'y': 1, 'w': 1})
 gear_ratio = rospy.get_param('gear_ratio', (
 	{'x': 0.25, 'y': 0.25, 'w': 0.25},
 	{'x': 0.5, 'y': 0.5, 'w': 0.5},
 	{'x': 0.75, 'y': 0.75, 'w': 0.75},
 	{'x': 1, 'y': 1, 'w': 1}
 ))
-setpoint_limits = rospy.get_param('setpoint_limits', {'x': 1, 'y': 1, 'w': 1})
 
 setpoint = {'x': 0.0,'y': 0.0,'w': 0.0}
 gear = 0
+update = 0
 
 def joy_cb(msg):
-    global gear
-    global setpoint    
+	global gear
+	global setpoint
+	global update
    
-    if msg.buttons[3]:
-        gear = 3
-    if msg.buttons[2]:
-        gear = 2
-    if msg.buttons[1]:
-        gear = 1
-    if msg.buttons[0]:
-        gear = 0
+	if msg.buttons[3]:
+		gear = 3
+	if msg.buttons[2]:
+		gear = 2
+	if msg.buttons[1]:
+		gear = 1
+	if msg.buttons[0]:
+		gear = 0
 
-    setpoint['x'] = msg.axes[0] * gear_ratio[gear]['x']
-    setpoint['y'] = msg.axes[1] * gear_ratio[gear]['y']
-    setpoint['w'] = msg.axes[2] * gear_ratio[gear]['w']
+	setpoint['x'] = msg.axes[1] * setpoint_scale['x'] * gear_ratio[gear]['x']
+	setpoint['y'] = msg.axes[0] * setpoint_scale['y'] * gear_ratio[gear]['y']
+	setpoint['w'] = msg.axes[2] * setpoint_scale['w'] * gear_ratio[gear]['w']
+	
+	update = 1
 
-def main():    
-    # Initialize ROS stuff
-    rospy.init_node("teleop_joy")
-    r = rospy.Rate(20) # 20hz
-    
-    pubVelocity = rospy.Publisher('/' + robot + '/velocity', Velocity)
-    pubVelocity.publish(Velocity(0.0, 0.0, 0.0))
-    
-    subJoy = rospy.Subscriber("/joy", Joy, joy_cb)
-    
-    while not rospy.is_shutdown():
-        print setpoint
-        pubVelocity.publish(Velocity(setpoint['x'], setpoint['y'], setpoint['w']))
-        r.sleep()
+def main():
+	global update
+	
+	# Initialize ROS stuff
+	rospy.init_node("teleop_joy")
+	r = rospy.Rate(20) # 20hz
+	
+	pubVelocity = rospy.Publisher(topic, Velocity)
+	pubVelocity.publish(Velocity(0.0, 0.0, 0.0))
+	
+	subJoy = rospy.Subscriber("/joy", Joy, joy_cb)
+	
+	while not rospy.is_shutdown():
+		update = 0
+		print setpoint
+		pubVelocity.publish(Velocity(setpoint['x'], setpoint['y'], setpoint['w']))
+		r.sleep()
 
-    # Stop the robot
-    pubVelocity.publish(Velocity(0.0, 0.0, 0.0))
+	# Stop the robot
+	pubVelocity.publish(Velocity(0.0, 0.0, 0.0))
 
 
 # Call the 'main' function when this script is executed
 if __name__ == "__main__":
-    try: main()
-    except rospy.ROSInterruptException: pass
+	try: main()
+	except rospy.ROSInterruptException: pass
 
